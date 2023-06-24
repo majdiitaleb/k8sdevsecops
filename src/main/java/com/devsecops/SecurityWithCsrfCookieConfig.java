8038e28cbe5ee8eb64e461744393129e626d746c
majdiitaleb <majditaleb@gmail.com>
2023-06-24 03:01:54 +0200

    qsdqsd

--- src/main/java/com/devsecops/SecurityWithCsrfCookieConfig.java
+++ src/main/java/com/devsecops/SecurityWithCsrfCookieConfig.java
@@ -4,15 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityWithCsrfCookieConfig  {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-        return httpSecurity.build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+        return http.build();
     }
 }
