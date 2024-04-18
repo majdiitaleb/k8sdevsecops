274e8ff198e7424ca7ec6e43196fd2499ab93a64
majdi <majditaleb@gmail.com>
2024-04-18 12:34:44 +0200

    remove unsued

--- src/main/java/com/devsecops/SecurityWithCsrfCookieConfig.java
+++ src/main/java/com/devsecops/SecurityWithCsrfCookieConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
 @Configuration
 public class SecurityWithCsrfCookieConfig  {
