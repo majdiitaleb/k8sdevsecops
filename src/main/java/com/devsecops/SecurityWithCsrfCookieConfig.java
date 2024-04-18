323d3569073ebbb08d99945670ef7947dc2e2f66
majdi <majditaleb@gmail.com>
2024-04-18 12:51:16 +0200

    sdqsd

--- src/main/java/com/devsecops/SecurityWithCsrfCookieConfig.java
+++ src/main/java/com/devsecops/SecurityWithCsrfCookieConfig.java
@@ -3,14 +3,19 @@ package com.devsecops;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityWithCsrfCookieConfig  {
+@EnableWebSecurity
+public class SecurityWithCsrfCookieConfig {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // ...
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
